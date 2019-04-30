e56d291bcb11bd2ae420eec37c9debd63e9f10d1
caphhe <caphhe1994@gmail.com>
2019-04-30 15:56:36 +0200

    The basic test works and is logged into a csv

---
+++ MCI2 Uebung1/Assets/Scripts/CSVLogger.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Text;
+using System;
+
+public class CSVLogger : MonoBehaviour
+{
+	public string delimiter = ";";
+
+	private StreamWriter tsw;
+
+	
+	// Start is called before the first frame update
+    void Start()
+    {
+		Setup();
+    }
+
+	private void Setup()
+	{
+		string filePath = Application.dataPath + "/Resources/" + "TestSession.csv"; // + DateTime.Now.ToShortTimeString() + ".csv"
+		//File.CreateText(filePath );
+		tsw = new StreamWriter(filePath);
+		string[] headRow = { "Width", "Distance", "Average Time", "Error Rate", "Clicks", "Tester", "Time Stamp"};
+		WriteLineToCSV(headRow);
+	}
+
+	public void LogToCSV (float width, float distance, float aTime, float errorRate, int testSize, string name, float time)
+	{
+		string[] lineData = {width.ToString(), distance.ToString(), aTime.ToString(), errorRate.ToString(), testSize.ToString(), name, time.ToString() };
+		WriteLineToCSV(lineData);
+	}
+
+	private void WriteLineToCSV(string[] lineData)
+	{
+		string line = string.Join(delimiter, lineData);
+		tsw.WriteLine(line);
+		tsw.Flush();
+	}
+}
--- MCI2 Uebung1/Assets/Scripts/TestManager.cs
+++ MCI2 Uebung1/Assets/Scripts/TestManager.cs
@@ -1,26 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
 
 public class TestManager : MonoBehaviour
 {
 
 	public float distanceMM;
 	public float widthMM;
-	public int retrysPerSetup;
-	
+	//public int retrysPerSetup;
+	//public test definition;
 
-
+	public InputField nameField;
+	public Button leftButton;
+	public Button rightButton;
+		
 	private float mmToPixel;
-	private List<float> time;
-	private List<bool> hit;
-	
+	private List<float> times = new List<float>();
+	private List<bool> hits = new List<bool>();
+	private bool hitLeft;
+	private float timeStampLastHit;
+	private CSVLogger logger;
+	private bool firstHit = true;
 	
 	
 	// Start is called before the first frame update
     void Start()
     {
-        
+		ToggleButton();
+		logger = gameObject.GetComponent<CSVLogger>();
     }
 
     // Update is called once per frame
@@ -29,20 +37,130 @@ public class TestManager : MonoBehaviour
         
     }
 
-	public void HitLeft()
+	public void LeftHit()
 	{
-
+		if (hitLeft)
+		{
+			Hit();		
+		}
+		else
+		{
+			Miss();
+		}
+		ToggleButton();
 	}
 
-	public void HitRight()
+	public void RightHit()
 	{
-
+		if (hitLeft)
+		{
+			Miss();
+		}
+		else
+		{
+			Hit();
+		}
+		ToggleButton();
 	}
 
-	public void HitBackground()
+	public void BackgroundHit()
 	{
-
+		Miss();
+		ToggleButton();
 	}
+
+	public void EndTest ()
+	{
+		if (hits.Count == 0)
+		{
+			ResetTest();
+			return;
+		}
+
+		float averageTime = 0;
+		float errorRate = 0;
+		for (int i = 0; i < times.Count; i++)
+		{
+			averageTime += times[i];
+			if (hits[i])
+			{
+				errorRate++;
+			}
+		}
+		averageTime = averageTime / times.Count;
+		Debug.Log(errorRate + " | " + hits.Count);
+		errorRate = 1 - (errorRate / hits.Count);
+		Debug.Log("Time Average: " + averageTime + " | error Rate: " + errorRate);
+		logger.LogToCSV(0, 0, averageTime, errorRate, hits.Count, "testName", Time.time);
+		ResetTest();
+	}
+
+	private void ResetTest()
+	{
+		times = new List<float>();
+		hits = new List<bool>();
+		firstHit = true;
+	}
+
+	private void ToggleButton()
+	{
+		hitLeft = !hitLeft;
+		ColorBlock leftColors = leftButton.colors;
+		ColorBlock rightColors = rightButton.colors;
+
+		if (hitLeft)
+		{
+			leftColors = SetColor(leftColors, Color.green);
+			rightColors = SetColor(rightColors, Color.red);
+		}
+		else
+		{
+
+			leftColors = SetColor(leftColors, Color.red);
+			rightColors = SetColor(rightColors, Color.green);
+		}
+
+		leftButton.colors = leftColors;
+		rightButton.colors = rightColors;
+		timeStampLastHit = Time.time;
+	}
+
+	private ColorBlock SetColor (ColorBlock block, Color c)
+	{
+		block.normalColor = c;
+		block.highlightedColor = c;
+		block.selectedColor = c;
+		return block;
+	}
+
+	private void Hit()
+	{
+		Debug.Log("Hit");
+		if (firstHit)
+		{
+			firstHit = false;
+			timeStampLastHit = Time.time;
+			return;
+		}
+		Debug.Log(timeStampLastHit - Time.time);
+		hits.Add(true);
+		times.Add(Time.time - timeStampLastHit);
+	}
+
+	private void Miss()
+	{
+		Debug.Log("Miss");
+		if (firstHit)
+		{
+			firstHit = false;
+			timeStampLastHit = Time.time;
+			return;
+		}
+		Debug.Log(timeStampLastHit - Time.time);
+		hits.Add(false);
+		times.Add(Time.time - timeStampLastHit);
+	}
+	
 
 
 	/*
@@ -50,5 +168,6 @@ public class TestManager : MonoBehaviour
 	 * Log one csv line per test setup: width + distance + Average Time to switch + Error rate in % +  Tester name
 	 * On miss -> directly switch to next target
 	 * 
+	 * What is the time of the first button press
 	 * */
 }
